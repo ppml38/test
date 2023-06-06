a7f4ccc0fc0e307658236429a1b6db919b4c087a
Liju <ppml38@gmail.com>
2023-06-06 23:58:00 +0530

    Added simple homepage

---
+++ res/js/script.js
@@ -0,0 +1,10 @@
+function showapps()
+{
+	document.getElementById("blurbg").style.display="block";
+	document.getElementById("appcontainer").style.display="inline-block";
+}
+function hideapps()
+{
+	document.getElementById("appcontainer").style.display="none";
+	document.getElementById("blurbg").style.display="none";
+}
